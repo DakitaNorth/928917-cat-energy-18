ac202c1e94666bfc592515e93c378edee5e0e34a
Dakita <garryant3120@yandex.ru>
2020-01-25 01:27:22 +0300

    Доработал проект,внес финальные правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+var popup = document.querySelector(".main-nav");
+var popup_button = document.querySelector(".page-header__button");
+var menu_button = document.querySelector(".page-header__button");
+
+popup_button.addEventListener("click",function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("main_open");
+    menu_button.classList.toggle("page-header__button--close");
+});
